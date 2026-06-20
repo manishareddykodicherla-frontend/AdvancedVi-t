7aaf999651d6455b3098821c72d174a52f01b7ae
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-06-19 19:16:51 -0700

    issue fixed

--- src/app/Settings/page.tsx
+++ src/app/Settings/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Settings from './Settings'
 import Sidebar from '../components/Sidebar'
+import Searchbar from '../components/Searchbar'
+import Styles from "../globals.css"
 export default function page() {
   return (
     <div>
+      <header className="header">
+        <Searchbar/>
+      </header>
       <Sidebar/>
       <Settings/></div>
   )
