012553148d25bcf0a5846e4457a89955c399ce29
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-06-22 15:41:44 -0700

    issue fixed

--- src/app/player/[id]/Player.tsx
+++ src/app/player/[id]/Player.tsx
@@ -1,22 +1,48 @@
- "use client"
- import {useSelector} from "react-redux";
- import Styles from "./Player.module.css";
- export  default function Player({id}:{id:string}){
-    const books = useSelector((state:any)=>state.books.allBooks ||[]);
-    const book =books.find((item:any)=>item.id?.toString()=== id);
-    console.log("player ID:", id)
-    console.log("Books:",books)
-    if (!book){
-        return <div>Loading Player.......</div>
+import React from 'react'
+import { useRef, useState} from "react";
+import {useSelector} from "react-redux";
+import { IoPlay, IoPause, IoPlayBack,IoPlayForward} from "react-icons/io5";
+import Styles from "./Player.module.css";
+export default function Player({id}:{id:string}) {
+    const audioRef= useRef<HTMLAudioElement>(null);
+    const [isPlaying,setIsPlaying]= useState(false);
+    const books= useSelector((state:any)=>state.books.allBooks||[]);
+    const book =books.find((item:any)=>item.id?.toString()===id);
+    if(!book){
+        return<div>Loading Player.......</div>
     }
-    return (
+    const handlePlayPause=()=>{
+        if(!audioRef.current) return;
+        if(isPlaying){
+            audioRef.current.pause();
+        }
+        else{
+            audioRef.current.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+    const handleBackward=()=>{
+        if(!audioRef.current) return;
+        audioRef.current.currentTime-=10;
+    }
+    const handleForward=()=>{
+        if(!audioRef.current) return;
+        audioRef.current.currentTime +=10;
+    }
+  return (
+    
         <main className={Styles.player}>
-            <h1> {book.title}</h1>
+            <h1>{book.title}</h1>
             <p>{book.summary}</p>
-            <audio controls autoPlay className={Styles.audio}>
+            <audio ref={audioRef}>
                 <source src={book.audioLink} type="audio/mpeg"/>
             </audio>
-
+<div className={Styles.controls}>
+    <button onClick={handleBackward}><IoPlayBack/></button>
+    <button onClick={handlePlayPause}>{isPlaying?<IoPause/>:<IoPlay/>}</button>
+    <button onClick={handleForward}> <IoPlayForward/></button>
+</div>
         </main>
-    )
- }
+    
+  )
+}
